7cadcc0b82096a93d62b78cd9e827c604232f337
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-05-16 21:04:41 -0400

    Title and splash touch-ups
    Assorted minor fixes

--- TitleScreen2D.gd
+++ TitleScreen2D.gd
@@ -8,11 +8,11 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$TextureRect/RichTextLabel.text = "START THE GAME"
+	$TextureRect/RichTextLabel.text = "Welcome to Pellet-Man.\nSTART THE GAME."
 	yield(get_tree().create_timer(10), "timeout")# Replace with function body.
-	$TextureRect/RichTextLabel.text = "Can't figure it out???"
+	$TextureRect/RichTextLabel.text = "PRESS THE START BUTTON"
 	yield(get_tree().create_timer(5), "timeout")
-	$TextureRect/RichTextLabel.text = "Can't figure it out?? Press Y to fly...or A/X to grapple...or you could climb..."
+	$TextureRect/RichTextLabel.text = "The 'REAL' Start Button"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
