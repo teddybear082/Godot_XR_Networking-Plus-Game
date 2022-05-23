d1e84aff317a2c913d64f9a6b9aad42176a5588f
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2022-05-23 07:17:26 -0400

    SilentWolf updates

--- GodotSplash.gd
+++ GodotSplash.gd
@@ -4,11 +4,15 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var apiKey = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	SilentWolf.configure({"api_key": "fOJ6gFZ8dP2p2whqdgiFp5cUO1dO11zP11QP27AM", "game_id": "Pellet-Man", "game_version": "1.0.6","log_level": 1})
+	var f = File.new()
+	f.open("res://swAPIKey.env", File.READ)
+	self.apiKey = f.get_line()
+	f.close()
+	SilentWolf.configure({"api_key": self.apiKey, "game_id": "Pellet-Man", "game_version": "1.0.6","log_level": 1})
 	preload("res://TitleScreen.tscn")
 	yield(get_tree().create_timer(6), "timeout") # Replace with function body.
 	get_tree().change_scene("res://TitleScreen.tscn")
